9ae0c1b44eac53226a589eaf787fdfb9b737b169
Huy Le <ing.huyle@gmail.com>
2024-10-11 14:28:45 +0200

    onMouse to onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './styles/App.scss';
 import $ from 'jquery';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const keys = [
   'q', 'w', 'e',
@@ -10,8 +10,9 @@ const keys = [
 
 const App = () => {
   const [text, setText] = useState('');
+  const timeoutRef = useRef(null);
 
-  const handleMouseDown = (event) => {
+  const handleClick = (event) => {
     setText(event.target.id);
 
     const audio = new Audio();
@@ -20,13 +21,17 @@ const App = () => {
 
     $(`#${event.target.id}`).removeClass('gray');
     $(`#${event.target.id}`).addClass('active');
-  }
 
-  const handleMouseUp = (event) => {
-    $(`#${event.target.id}`).addClass('gray');
-    $(`#${event.target.id}`).removeClass('active');
-  }
+    if (timeoutRef) {
+      clearTimeout(timeoutRef.current)
+    }
 
+    timeoutRef.current = setTimeout(() => {
+      $(`#${event.target.id}`).addClass('gray');
+      $(`#${event.target.id}`).removeClass('active');
+    }, 100);
+  }
+  
   const handleKeyDown = (event) => {
     if (keys.includes(event.key)) {
       const targetAudio = $(`#${event.key.toUpperCase()}`)[0];
@@ -67,63 +72,63 @@ const App = () => {
       <h1>Drum Machine</h1>
       <div id='display' className="gray">{text}</div>
       <div className='drum-pads'>
-        <div className='drum-pad gray' id='Heater-1' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>Q
+        <div className='drum-pad gray' id='Heater-1' onClick={handleClick}>Q
           <audio
             className='clip'
             id='Q'
             src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Heater-2' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>W
+        <div className='drum-pad gray' id='Heater-2' onClick={handleClick}>W
           <audio
             className='clip'
             id='W'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Heater-3' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>E
+        <div className='drum-pad gray' id='Heater-3' onClick={handleClick}>E
           <audio
             className='clip'
             id='E'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Heater-4' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>A
+        <div className='drum-pad gray' id='Heater-4' onClick={handleClick}>A
           <audio
             className='clip'
             id='A'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Clap' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>S
+        <div className='drum-pad gray' id='Clap' onClick={handleClick}>S
           <audio
             className='clip'
             id='S'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Open-HH' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>D
+        <div className='drum-pad gray' id='Open-HH' onClick={handleClick}>D
           <audio
             className='clip'
             id='D'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id="Kick-n-Hat" onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>Y
+        <div className='drum-pad gray' id="Kick-n-Hat" onClick={handleClick}>Y
           <audio
             className='clip'
             id='Y'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Kick' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>X
+        <div className='drum-pad gray' id='Kick' onClick={handleClick}>X
           <audio
             className='clip'
             id='X'
             src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'>
           </audio>
         </div>
-        <div className='drum-pad gray' id='Closed-HH' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>C
+        <div className='drum-pad gray' id='Closed-HH' onClick={handleClick}>C
           <audio
             className='clip'
             id='C'
