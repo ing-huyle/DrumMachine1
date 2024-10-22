be2d416dedd9ff4a17a1443edb55eca490cc2af2
Huy Le <ing.huyle@gmail.com>
2024-10-22 13:41:27 +0200

    optimized render

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,55 @@ import './styles/App.scss';
 import $ from 'jquery';
 import { useState, useEffect, useRef } from 'react';
 
-const keys = [
-  'Q', 'W', 'E',
-  'A', 'S', 'D',
-  'Y', 'X', 'C'
+const drumPads = [
+  {
+    id: 'Heater-1',
+    key: 'Q',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    id: 'Heater-2',
+    key: 'W',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'
+  },
+  {
+    id: 'Heater-3',
+    key: 'E',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'
+  },
+  {
+    id: 'Heater-4',
+    key: 'A',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'
+  },
+  {
+    id: 'Clap',
+    key: 'S',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'
+  },
+  {
+    id: 'Open-HH',
+    key: 'D',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'
+  },
+  {
+    id: 'Kick-n-Hat',
+    key: 'Y',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'
+  },
+  {
+    id: 'Kick',
+    key: 'X',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'
+  },
+  {
+    id: 'Closed-HH',
+    key: 'C',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'
+  }
 ];
+
+const keys = drumPads.map((drumPad => drumPad.key));
 
 const toggleClass = (elementId, addClass, removeClass) => {
   $(`#${elementId}`).addClass(addClass);
@@ -73,42 +117,14 @@ const App = () => {
       <h1>Drum Machine</h1>
       <div id='display' className="gray">{text}</div>
       <div className='drum-pads'>
-        <div className='drum-pad gray' id='Heater-1' onClick={handleClick}>
-          {keys[0]}
-          <audio id='Q' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Heater-2' onClick={handleClick}>
-          {keys[1]}
-          <audio id='W' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Heater-3' onClick={handleClick}>
-          {keys[2]}
-          <audio id='E' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Heater-4' onClick={handleClick}>
-          {keys[3]}
-          <audio id='A' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Clap' onClick={handleClick}>
-          {keys[4]}
-          <audio id='S' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Open-HH' onClick={handleClick}>
-          {keys[5]}
-          <audio id='D' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id="Kick-n-Hat" onClick={handleClick}>
-          {keys[6]}
-          <audio id='Y' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Kick' onClick={handleClick}>
-          {keys[7]}
-          <audio id='X' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3'></audio>
-        </div>
-        <div className='drum-pad gray' id='Closed-HH' onClick={handleClick}>
-          {keys[8]}
-          <audio id='C' src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3'></audio>
-        </div>
+        {
+          drumPads.map((drumPad => (
+            <div className='drum-pad gray' id={drumPad.id} key={drumPad.id} onClick={handleClick}>
+              {drumPad.key}
+              <audio id={drumPad.key} src={drumPad.src}></audio>
+            </div>
+          )))
+        }
       </div>
       <p>Coded by<a href='https://www.linkedin.com/in/ing-huyle' target='_blank'>ing.huyle</a><br/>
         Designed by freeCodeCamp
